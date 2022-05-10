2b7d1d6d7459b049e0150acdee4815ae8519c12b
chernybro <anvar-1234567@yandex.ru>
2022-05-10 10:49:59 +0300

    Implement logging, alert dialog

--- app/src/main/java/com/chernybro/wb21/MainActivity.kt
+++ app/src/main/java/com/chernybro/wb21/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chernybro.wb21.databinding.ActivityDialogBinding
 import com.chernybro.wb21.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,8 +18,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.fab.setOnClickListener { view ->
+        binding.fab.setOnClickListener {
             startActivity(Intent(this, DialogActivity::class.java))
         }
+    }
+
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.d("${this.localClassName} onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.d("${this.localClassName} onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("${this.localClassName} onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("${this.localClassName} onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("${this.localClassName} onDestroy")
     }
 }
